7446baf91b89db170c05305a8b8e3fe5a6a97152
rpu5040 <ruthvikuttarala@gmail.com>
2024-09-16 17:03:03 -0400

    Update write_ids.c

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "rpu5040";
+    char STUDENT_ID[] = "998209829";
 
     return 0 ;
 }
